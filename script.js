a572b2c155b990ebd7fa02fdd5b679ce3cb3a4b1
Alex <page.alexanderjohn@gmail.com>
2022-11-13 19:35:16 -0500

    Site is basically done
    Essentially done, may need final touches after deployment still

--- script.js
+++ script.js
@@ -2,31 +2,39 @@
 $(() => {        
     let heading = $('#heading')
     if ($(document).width() <= 753) {
-        heading.text("Make remote work");
         $('#hero').attr('src', './images/image-hero-mobile.png')
     }
 
     else {
-        heading.innerText = `Make \nremote work`;
         $('#hero').attr('src', './images/image-hero-desktop.png')
     }
 
     window.addEventListener('resize', () => {
         if ($(document).width() <= 753) {
-            heading.text("Make remote work");
+
             $('#hero').attr('src', './images/image-hero-mobile.png')
+
+            $('#nav_container').hide();
+
+            $('#veil').hide();
+
+            $('#menu_icn').show();
 
         }
 
         else {
-            heading.text('Make \nremote work');
             $('#hero').attr('src', './images/image-hero-desktop.png')
+
+            $('#close').hide();
+
+            $('#veil').hide();
 
             if ($('#nav_container').css('display') == 'none') {
                 $('#nav_container').css({
                     'display' : 'flex'
                 });
             }
+
         }
     });
 
@@ -37,58 +45,76 @@ $(() => {
 
         if ($('#feat_menu').css('display') == 'block') {
             // On medium or smaller screens
-            if ($(document).width() > 753) {
-                    if (element.includes($('#feat_btn')[0]) ||
-                        element.includes($('#features')[0]) ||
-                        element.includes($('.arrow')[0]) ||
-                        element.includes($('h3')[0]) ||
-                        element.includes($('li')[0])) {
-
-                    return
-                }
-                else {
-                    $('#feat_menu').hide()
-                }
+            if (element.includes($('#features')[0]) ||
+                element.includes($('#feat_menu')[0]) ||
+                element.includes($('#feat_arrow')[0]) ||
+                element.includes($('#company')[0]) ||
+                element.includes($('#comp_menu')[0]) ||
+                element.includes($('#comp_arrow')[0])) {
+                return
             }
+            else {
+                $('#feat_menu').hide();
+                $('#feat_arrow').attr('src','./images/icon-arrow-down.svg') 
+            }
+            
         }
 
         if ($('#comp_menu').css('display') == 'block') {
             // On medium or smaller screens
-            if ($(document).width() > 753) {
-                if (element.includes($('#comp_btn')[0]) ||
-                    element.includes($('#company')[0]) ||
-                    element.includes($('.arrow')[0]) ||
-                    element.includes($('h3')[0]) ||
-                    element.includes($('li')[0])) {
-
-                    return
-                }
-                else {
-                    $('#comp_menu').hide()
-                }
+            if (element.includes($('#company')[0]) ||
+                element.includes($('#comp_menu')[0]) ||
+                element.includes($('#comp_arrow')[0]) ||
+                element.includes($('#features')[0]) ||
+                element.includes($('#feat_menu')[0]) ||
+                element.includes($('#feat_arrow')[0])) {
+                return
             }
+            else {
+                $('#comp_menu').hide();
+                $('#comp_arrow').attr('src','./images/icon-arrow-down.svg') 
+            }
+            
         }
 
+    });
+
+    // Listens for scrolling
+    window.addEventListener("scroll", () => {
+        if ($(document).width() > 753) {
+            if ($('#feat_menu').css('display') != 'none' ||
+                $('#comp_menu').css('display') != 'none') {
+                    $('#feat_menu').hide();
+                    $('#comp_menu').hide();
+                    $('#feat_arrow').attr('src','./images/icon-arrow-down.svg')
+                    $('#comp_arrow').attr('src','./images/icon-arrow-down.svg')
+
+            }
+        }
     });
 });
 
 function features() {
     if ($('#feat_menu').css('display') == 'none') {
         $('#feat_menu').show();
+        $('#feat_arrow').attr('src','./images/icon-arrow-up.svg') 
     }
 
     else {
         $('#feat_menu').hide();
+        $('#feat_arrow').attr('src','./images/icon-arrow-down.svg') 
     }
 }
 
 function company() {
     if ($('#comp_menu').css('display') == 'none') {
         $('#comp_menu').show();
+        $('#comp_arrow').attr('src','./images/icon-arrow-up.svg') 
     }
 
     else {
         $('#comp_menu').hide();
+        $('#comp_arrow').attr('src', './images/icon-arrow-down.svg')
     }
 }
 
